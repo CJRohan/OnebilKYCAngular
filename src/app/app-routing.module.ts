ca9e6fb326639b0a52cbb6b028293b628b52c63c
CJ Rohan <rohancj997@gmail.com>
2020-07-24 12:27:37 +0530

    Update Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsInputFormComponent } from './details-input-form/details-input-form.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'get-details', component: DetailsInputFormComponent},
+  { path: 'list-customers', component: CustomersListComponent},
+  { path: 'update-customer', component: UpdateCustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsInputFormComponent } from './details-input-form/details-input-form.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { CustomersListComponent } from './customers-list/customers-list.component'
+import { NgxPaginationModule } from 'ngx-pagination';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsInputFormComponent,
+    HomeComponent,
+    CustomersListComponent,
+    UpdateCustomerComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule
+    
+
   ],
-  providers: [],
+  providers: [JsonPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
